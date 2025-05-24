69c7c18bc505cb16dea545a95ad6114113e97305
wangshiyu <2418521205@qq.com>
2025-05-24 14:43:06 +0800

    前端页面调整

--- src/main/resources/static/js/auth-helper.js
+++ src/main/resources/static/js/auth-helper.js
@@ -262,8 +262,20 @@
             const userActionsEl = document.getElementById('userActions');
             if (userActionsEl) {
                 userActionsEl.innerHTML = `
-                    <span class="navbar-text me-3">欢迎，${username}</span>
-                    <button class="btn btn-outline-danger" id="logoutBtn">退出登录</button>
+                    <div class="dropdown">
+                        <button class="btn btn-light dropdown-toggle d-flex align-items-center" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle me-1" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                            </svg>
+                            ${username}
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                            <li><a class="dropdown-item" href="/static/user-center.html#address-list">地址管理</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#" id="logoutBtn">退出登录</a></li>
+                        </ul>
+                    </div>
                 `;
                 
                 // 添加退出登录按钮事件
